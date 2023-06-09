d06171434f6ffb8c5ee2a0d296e925f10278283b
Vadim Shershen <vadim.shershen.97@mail.ru>
2023-06-09 12:21:58 +0300

    feat: add global assets and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
